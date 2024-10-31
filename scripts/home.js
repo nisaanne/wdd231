13666df5c0212a19c96cf068a868106a73c38779
nisaanne <youremail@nisaanne@byui.edu>
2024-10-31 09:19:10 -0700

    css header

---
+++ scripts/home.js
@@ -0,0 +1,7 @@
+{
+    var currentYear = new Date().getFullYear();
+    document.getElementById('current-year').textContent = currentYear;
+  
+    let lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;
+  }
