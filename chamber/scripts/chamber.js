3fbe28300d8f8917a8df87642aca4375293a8b9f
nisaanne <youremail@nisaanne@byui.edu>
2024-11-16 17:22:35 -0800

    fix directory location

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,13 @@
+var currentYear = new Date().getFullYear();
+    document.getElementById('current-year').textContent = currentYear;
+  
+let lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent = "This document was last modified on: " + lastModifiedDate;
+
+const mainnav = document.querySelector("#animateme")
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener("click", () =>{
+  mainnav.classList.toggle("show");
+  hambutton.classList.toggle("show");  
+});
