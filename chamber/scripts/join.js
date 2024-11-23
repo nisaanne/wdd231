c50e3a38edabe086f1a9227f70b54a304d8fba74
nisaanne <youremail@nisaanne@byui.edu>
2024-11-23 14:40:09 -0800

    reset form

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -13,37 +13,8 @@ hambutton.addEventListener("click", () =>{
   hambutton.classList.toggle("show");  
 });
 
-const currentURL = window.location.href;
-
-
-const everything=currentURL.split("?")
-
-
-let formData = everything[1].split("&")
-
-
-function show(cup) {
-   
-    formData.forEach((element)=> {
-        
-        if (element.startsWith(cup)){
-            result=element.split('=')[1].replace("%40" , "@")
-            
-        }
-    })
-    return(result)
-}
-
-const showInfo = document.querySelector('#results')
-showInfo.innerHTML = `
-<p>Appointment for ${show("first")} ${show("last")}</p>
-<p>Proxy ${show('ordinance')} on ${show('fecha')} in the ${show('location')} Temple. </p>
-<p>Your Phone: ${show("phone")}</p>
-<p>Your Email: <a href="mailto:${show("email")}">${show("email")}</a>
 
 `
-
-  
 });
 
 
