46d54b3a99731027380c8549680832506d58522a
nisaanne <youremail@nisaanne@byui.edu>
2024-12-06 06:56:51 -0800

    format timestamp

--- purrfectpal/scripts/thanks.js
+++ purrfectpal/scripts/thanks.js
@@ -21,19 +21,33 @@ document.addEventListener("DOMContentLoaded", () => {
         return result;
       }
   
+      function formatTimestamp(timestamp) {
+        const date = new Date(timestamp);
+        const options = { 
+            weekday: 'long', 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric', 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            second: '2-digit' 
+        };
+        return date.toLocaleDateString('en-US', options);
+      }
+  
       const showInfo = document.querySelector('#results');
       if (showInfo) {
         showInfo.innerHTML = `
           <p>Name: ${show("first")} ${show("last")}</p>
           <p>Email: ${show("email")}</p>
           <p>Phone number: ${show("phone")}</p>
-          <p>Timestamp: ${show("timestamp")}</p>
+          <p>Timestamp: ${formatTimestamp(show("timestamp"))}</p>
         `;
       }
     } else {
       console.log("No query parameters found.");
     }
-  });
-  
+});
+
 
 
